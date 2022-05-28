25fc028b7120df75ccdd8eaae339e658c8981e86
Kathy <byc233@terpmail.umd.edu>
2022-05-28 00:57:03 -0400

    add android settings

--- stybyc/lib/main.dart
+++ stybyc/lib/main.dart
@@ -1,10 +1,11 @@
 import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 import 'package:flutter/material.dart';
 import 'package:stybyc/auth/decision_tree.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   //await UserPreferences.init();
 
   runApp(const MyApp());
